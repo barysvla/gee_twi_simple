5b3c1ec3c7f0d67d15f17d304cb0181484a225f9
barysvla <vladislav.baryshev@outlook.com>
2025-08-17 15:55:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import ee
 import geemap
 
-# Import vlastních modulů
+# Import custom modules
 from scripts.flow_accumulation import compute_flow_accumulation
 from scripts.slope import compute_slope
 from scripts.twi import compute_twi
 from scripts.visualization import visualize_map
-#from scripts.export import export_to_drive, export_to_asset
+# from scripts.export import export_to_drive, export_to_asset
 
-# !Inicializace GEE!
-ee.Initialize(project = 'TADY zadat ID projektu')
+# !GEE initialization!
+ee.Initialize(project = 'Enter the GEE project ID here')
 
-# !Definice zájmového území!
+# !Define area of interest (AOI)!
 geometry = ee.Geometry.Rectangle([14.2, 50.0, 14.6, 50.2])
 
-# Získání středu polygonu a nastavení zoomu
+# Get polygon centroid and configure zoom
 center = geometry.centroid().coordinates().getInfo()
 
-# Načtení DEM
+# Load DEM
 dataset_MERIT = ee.Image("MERIT/Hydro/v1_0_1")
 dem = dataset_MERIT.select("elv").clip(geometry)
 
-# Výpočet jednotlivých vrstev
+# Compute individual layers
 flow_accumulation = compute_flow_accumulation(dem)
 slope = compute_slope(dem)
 twi = compute_twi(flow_accumulation, slope)
 
-# Kombinace vrstev
-#out = dem.addBands(twi) #.addBands(flow_accumulation).addBands(slope)
+# Combine layers
+# out = dem.addBands(twi)  # .addBands(flow_accumulation).addBands(slope)
 
-# Vizualizace
+# Visualization
 vis_params_twi = {
     "bands": ["TWI_scaled"],
     "min": -529168144.8390943,
@@ -37,24 +37,24 @@ vis_params_twi = {
     "opacity": 1,
     "palette": ["#ff0000", "#ffa500", "#ffff00", "#90ee90", "#0000ff"]
 }
-#vis_params_slope = {
-#    "bands": ["Slope"],
-#    "min": 0,
-#    "max": 90,
-#    "palette": ["yellow", "red"]
-#}
-#vis_params_dem = {
-#    "bands": ["elv"],
-#    "min": 0,
-#    "max": 3000,
-#    "palette": ["black", "white"]
-#}
+# vis_params_slope = {
+#     "bands": ["Slope"],
+#     "min": 0,
+#     "max": 90,
+#     "palette": ["yellow", "red"]
+# }
+# vis_params_dem = {
+#     "bands": ["elv"],
+#     "min": 0,
+#     "max": 3000,
+#     "palette": ["black", "white"]
+# }
 
-# Vytvoření mapy
+# Create the map
 Map = visualize_map([
-    (twi, vis_params_twi, "TWI") #,
-   # (out.select("Slope"), vis_params_slope, "Slope"),
-   # (out.select("elv"), vis_params_dem, "Elevation")
+    (twi, vis_params_twi, "TWI")  # ,
+    # (out.select("Slope"), vis_params_slope, "Slope"),
+    # (out.select("elv"), vis_params_dem, "Elevation")
 ])
 
 Map.setCenter(center[0], center[1], zoom=12)
